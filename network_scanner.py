c7b304811747774f608dab3af973b0686b20ba00
Sergey Nazemtsev <42sdn@mail.ru>
2021-07-22 18:34:44 +0700

    update

--- network_scanner.py
+++ network_scanner.py
@@ -10,8 +10,7 @@ def scan(ip):
     for element in answered_list:
         print(element[1].psrc)
         print(element[1].hwsrc)
-        print("*")
-        print("****************************")
+        print("***************************")
 
 
 scan("10.0.2.1/24")
