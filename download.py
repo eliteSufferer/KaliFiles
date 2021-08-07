6a3c5c69aa1921cd121d85fe5a5e10e4596a9aa9
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-07 22:03:44 +0700

    Work with files + download from url

---
+++ download.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+
+download("https://wroom.ru/i/cars2/ferrari_roma_1.jpg")
